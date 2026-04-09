05e3d45d16a5772f66c96859c12c24a657ec1fcf
engrtanveerbaig-tech <engr.tanveerbaig@gmail.com>
2026-04-09 15:39:21 +0300

    fix: proper railway mysql connect

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,18 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection(process.env.MYSQL_PUBLIC_URL);
+// Parse Railway URL
+const url = new URL(process.env.MYSQL_PUBLIC_URL);
+
+const db = mysql.createConnection({
+  host: url.hostname,
+  user: url.username,
+  password: url.password,
+  database: url.pathname.replace("/", ""),
+  port: url.port,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 db.connect((err) => {
   if (err) {
