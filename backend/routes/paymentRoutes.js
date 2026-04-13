4017063dc87d8df42fde9f1cb8fd095cb2e11ca2
engrtanveerbaig-tech <engr.tanveerbaig@gmail.com>
2026-04-13 14:51:20 +0300

    debug the issue

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -165,7 +165,7 @@ VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
         }
 
         console.error(err);
-        res.status(500).send("Server error");
+        res.status(500).send(err.message);
 
     } finally {
         conn.release();
